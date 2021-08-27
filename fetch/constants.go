d14ce82cd70a3cf59c77748022755f937f9dd552
dreese33 <dylanjacobreese@gatech.edu>
2021-08-27 19:55:25 -0400

    initial attempt to create a Go package

---
+++ fetch/constants.go
@@ -0,0 +1,4 @@
+package fetch
+
+// API Urls
+const PUG_API = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/"
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/dreese33/genomics/fetch"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("go" + "lang")
+}
